12bc311b02af3da793388d987c4c01c12bfe62f9
Unknown <rifawisdom@gmail.com>
2018-08-07 19:18:30 +0800

    done user authentication

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+	def new
+    end
+
+    def create
+	    user = User.new(user_params)
+	  if user.save
+	    session[:user_id] = user.id
+	    redirect_to '/'
+	  else
+	    redirect_to '/signup'
+	  	flash[:danger] = "Opps! There is something wrong.Please try again"
+	  end
+    end   
+
+
+
+	private
+	def user_params
+	  params.require(:user).permit(:name, :email, :password, :password_confirmation, :age, :gender, :phone)
+	end
+	
+end
